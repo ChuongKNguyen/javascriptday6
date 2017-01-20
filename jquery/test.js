fa277033667b1ac34c1e53c9b6a26c96bc3af473
Chuong Nguyen <tinchuong@yahoo.fr>
2017-01-20 15:22:56 -0500

    sliding function added

--- jquery/test.js
+++ jquery/test.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
-    $("#circleRow").click(function(){
-        $("#panel").slideDown("slow");
-    });
+    $( document ).click(function() {
+
+    	sliding(".circleRow");
+    	sliding(".squareRow");
+
+   }); 
 });
+
+
+//  $( ".circleRow" ).animate({
+//    "margin-left": "50%"
+//  }, 5000)
+// })
+
+function sliding(part){
+
+	var position = $(part).css("left");
+	console.log(position)
+
+
+	if (position == "8px" || position == "0px"){
+		$(part).animate({left: 650}, 500,
+    			function complete() {
+    				sliding(part);
+    		});
+	}
+	else {
+		$(part).animate({left: 0}, 500,
+    			function complete() {
+    				sliding(part);
+			});    	
+	}
+}
